5844fe54bccace5cc0d4a1ad1719f2738ede2d70
kareemkamal11 <sefi31009853@f-eng.tanta.edu.eg>
2024-10-30 18:04:54 +0300

    Add news fetching methods to HomeCubit for various queries

--- lib/features/home/view_model/home_cubit.dart
+++ lib/features/home/view_model/home_cubit.dart
@@ -1,13 +1,55 @@
 import 'dart:convert';
+import 'home_state.dart';
 
 import 'package:http/http.dart' as http;
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
+  final String _baseUrl = 'https://api.mediastack.com/v1/news';
+  final String _apiKey = '89f8b6602b9a8ca66bcc282d2be421ac';
+
+  /// جلب كافة الأخبار
+  Future<List<dynamic>> fetchAllNews() async {
+    final url = Uri.parse('$_baseUrl?access_key=$_apiKey');
+    return await _getNewsData(url);
+  }
+
+  /// جلب الأخبار بناءً على قسم معين
+  Future<List<dynamic>> fetchNewsByCategory(String category) async {
+    final url = Uri.parse('$_baseUrl?access_key=$_apiKey&categories=$category');
+    return await _getNewsData(url);
+  }
+
+  /// جلب الأخبار بناءً على مصدر معين
+  Future<List<dynamic>> fetchNewsBySource(String source) async {
+    final url = Uri.parse('$_baseUrl?access_key=$_apiKey&sources=$source');
+    return await _getNewsData(url);
+  }
+
+  /// البحث عن الأخبار بكلمات معينة
+  Future<List<dynamic>> searchNews(String keywords) async {
+    final url = Uri.parse('$_baseUrl?access_key=$_apiKey&keywords=$keywords');
+    return await _getNewsData(url);
+  }
+
+  /// دالة مساعدة لتنفيذ الطلبات العامة وتجنب تكرار الكود
+  Future<List<dynamic>> _getNewsData(Uri url) async {
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return data['data'];
+      } else {
+        print('Failed to load news. Status code: ${response.statusCode}');
+        return [];
+      }
+    } catch (e) {
+      print('An error occurred: $e');
+      return [];
+    }
+  }
 }
 
 const apiKey = '89f8b6602b9a8ca66bcc282d2be421ac';
